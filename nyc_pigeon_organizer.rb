1fd67d809b8d5e1856ab244187e46033067a7b14
Jeremy Winn <jeremyjwinn@gmail.com>
2020-06-26 18:29:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key,value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
+      end 
     end
   end 
   final_array
