8f46b8e013fb8af9d759cb469f1888bb47d699c9
Jeremy Winn <jeremyjwinn@gmail.com>
2020-06-26 17:50:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,9 @@
 def nyc_pigeon_organizer(data)
-  
+  pigeon_data = {
+    "Alex" => {
+      :color => ["brown", "white"]
+      :gender => [:"male"]
+      :lives => ["Central Park"]
+    }
+  }
 end
